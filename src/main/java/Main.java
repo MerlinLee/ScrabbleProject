a6030157e08a1e9fda605576a33305e401842fae
Merlin Lee <merlinlee@users.noreply.github.com>
2018-09-25 15:52:34 +1000

    commit: important network modular!

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
+import org.apache.log4j.PropertyConfigurator;
 import scrabble.server.controllers.controlcenter.ControlCenter;
 
 public class Main {
     public static void main(String[] args){
-        ControlCenter controlCenter = new ControlCenter();
+        PropertyConfigurator.configure(Main.class.getResourceAsStream("log4j.properties"));
+        new Thread(new ControlCenter()).start();
+
     }
 }
