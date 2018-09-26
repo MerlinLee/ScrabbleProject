53a35b43b39cd03c23376b7d81c51cde1e3a78ac
Merlin Lee <merlinlee@users.noreply.github.com>
2018-09-26 10:22:51 +1000

    commit: important network modular! add Thread and Pipe. Well done

--- src/main/java/scrabble/server/controllers/net/NetSendMsg.java
+++ src/main/java/scrabble/server/controllers/net/NetSendMsg.java
@@ -4,6 +4,7 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Enumeration;
 import java.util.Hashtable;
 
 public class NetSendMsg implements Runnable {
@@ -16,12 +17,21 @@ public class NetSendMsg implements Runnable {
     }
     @Override
     public void run() {
-        try {
-            PrintWriter printWriter = new PrintWriter(new DataOutputStream(client.getOutputStream()));
-            printWriter.println(message);
-            printWriter.flush();
-        } catch (IOException e) {
-            e.printStackTrace();
+        sendBroadcastMsg("");
+    }
+
+    private void sendBroadcastMsg(String msg){
+        synchronized (clientNameTable){
+            for(Enumeration enu=clientNameTable.elements();enu.hasMoreElements();){
+                client = (Socket)enu.nextElement();
+                try {
+                    PrintWriter printWriter = new PrintWriter(new DataOutputStream(client.getOutputStream()));
+                    printWriter.println("hello");
+                    printWriter.flush();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
         }
     }
 }
