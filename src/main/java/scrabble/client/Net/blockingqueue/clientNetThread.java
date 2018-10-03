a7f857792e4f6014768ef2dacb361da412a5fb36
Merlin Lee <merlinlee@users.noreply.github.com>
2018-10-03 20:23:57 +1000

    Merge branch 'version_0.5' of https://github.com/Himonokuroko/ScrabbleProject-1 into feature_version_0.82
    # Conflicts:
    #	src/main/java/scrabble/client/gui/GameLobbyWindow.java

--- src/main/java/scrabble/client/Net/blockingqueue/clientNetThread.java
+++ src/main/java/scrabble/client/Net/blockingqueue/clientNetThread.java
@@ -3,6 +3,7 @@ package scrabble.client.Net.blockingqueue;
 import scrabble.protocols.Pack;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
 import java.util.Hashtable;
@@ -48,6 +49,11 @@ public class clientNetThread implements Runnable {
         }
     }
     private void closeClient(){
+        try {
+            server.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         System.out.println("System shutdown!");
     }
 }
